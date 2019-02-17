6fb4e7c8e6772ee20795584c3722d0f744848d8f
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-17 21:35:48 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |str|
+    str.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
