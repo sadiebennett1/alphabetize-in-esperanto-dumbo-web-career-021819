e9a16d36ddc0af4367fe8cfca1b91c6a8083b418
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-17 21:42:10 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |str|
+    str.split("").map do |character|
+      esperanto.index(character)
+    end
+  end
 end
